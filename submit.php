a2e28bbb8cd0b0e95e137480d9ae3b04f574a1eb
Yudi Rosen <yudi42@gmail.com>
2011-09-01 17:39:36 +0000

    Edited submit page

--- submit.php
+++ submit.php
@@ -16,6 +16,9 @@ if(!$User->is_loggedin()) {
 $Template->ptitle = 'Submit Event';
 $Template->activelink = 'Submit Event';
 ?>
+
+<script type="text/javascript">$(function() {$("#event_date").datepicker({ dateFormat: 'DD, M d, yy' });});</script>
+
 <div id="fp_top">
 	<h1>Submit Event</h1>
 </div>
@@ -23,19 +26,30 @@ $Template->activelink = 'Submit Event';
 <form method="post" action="">
 	<div class="notifymsg">Please include as much information as possible about the event!</div>
 
-	Event Name:<br />
-	<input class="textbox" type="text" id="event_name" name="event_name" value=""  /><br /><br />
+	<div style="padding:8px;">
+		<strong>Event Name:</strong><br />
+		<input class="textbox" type="text" id="event_name" name="event_name" value=""  style="width:500px;" /><br /><br />
 
-	Event Date:<br />
-	<input class="textbox" type="text" id="event_date" name="event_date" value=""  /><br /><br />
+		<div class="left" style="width:300px;">
+		<strong>Event Date:</strong><br />
+			<input class="textbox" type="text" id="event_date" name="event_date" value=""  /><br /><br />
+		</div>
 
-	Event Location:<br />
-	<input class="textbox" type="text" id="event_location" name="event_location" value=""  /><br /><br />
+		<div class="left">
+		<strong>Event Time:</strong><br />
+			<input class="textbox" type="text" id="event_time" name="event_time" value=""  /><br /><br />
+		</div>
 
-	Event information:<br />
-	<i><b>Example:</b> event host, types of food served, description, contact information.</i>
-	<textarea name="quote" class="textarea" rows="8" cols="100"></textarea><br /><br />
+		<div class="clear"></div>
 
-	<input type="hidden" name="k" value="<?php echo $csrf->getKey(); ?>" />
-	<input class="button" type="submit" name="search" value="Submit!" />
+		<strong>Event Location:</strong><br />
+		<input class="textbox" type="text" id="event_location" name="event_location" value=""  /><br /><br />
+
+		<strong>Event information:</strong><br />
+		<i><b>Example:</b> event host, types of food served, description, contact information.</i>
+		<textarea name="quote" class="textarea" rows="8" cols="80" style="width:500px;"></textarea><br /><br />
+
+		<input type="hidden" name="k" value="<?php echo $csrf->getKey(); ?>" />
+		<input class="button" type="submit" name="search" value="Submit!" />
+	</div>
 </form>
