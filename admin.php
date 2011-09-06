9b93cb90004da1535e39cbb06566a2f3ac59ba66
Yudi Rosen <yudi42@gmail.com>
2011-09-06 03:04:59 +0000

    Started adming panel

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php define('IN_APP', true);
+/** 
+ * yFood
+ * admin.php - Site administration
+ *
+ * @author Yudi Rosen <yudi42@gmail.com>
+ * @package yFood application
+ */
+require_once('./global.inc.php');
+
+// We need to be an administrator:
+if(!$User->is_loggedin() || $_SESSION['mgroup'] != $config['mgroup']['admin']) {
+	header("Location: {$config['site_url']}");
+	die();
+}
+
+$Template->ptitle = 'Administration';
+?>
